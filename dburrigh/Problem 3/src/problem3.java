c65d3f2726c502b2f898ecfe93dbb0260150bfba
dburrigh <dburrigh@nebrwesleyan.edu>
2018-02-26 11:37:57 -0600

    Problem 1 and 3 are complete. Problem 2 is no bueno. Ask question

--- dburrigh/Problem 3/src/problem3.java
+++ dburrigh/Problem 3/src/problem3.java
@@ -2,22 +2,32 @@ import java.util.Scanner;
 
 public class problem3 {
 
-    public static void main(String[] args)throws Exception {
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String [] firstNames = {"Dallas","Bob"};
-        String [] lastNames = {"Burright","Johnson"};
-        int i =0;
+        String[] firstNames = {"dallas", "bob", "j"};
+        String[] lastNames = {"burright", "johnson","life"};
+        int i;
+        for (i = 0; i < firstNames.length; i++) {
+            System.out.print((firstNames[i].charAt(0)));
+            int j = 0;
+            int Letter = 0;
+            int count = 1;
+            while (j < Math.min(firstNames[i].length() - 1, lastNames[i].length()) && firstNames[i].charAt(j + 1) < lastNames[i].charAt(0)) {
+                System.out.print(firstNames[i].charAt(j + 1));
+                j++;
+                count++;
+            }
+            if(count + lastNames[i].length()<8) {
+                System.out.print(lastNames[i]);
+            }else{
 
-        do {
-            System.out.println(i.toLowerCase(firstNames));
-            i++;
-        }
-        while(i<7; i++){
-
-            if(firstNames[i]<lastNames[i-i]){
-                System.out.println(i.toLowerCase(firstNames));}
-                else{
-                System.out.print(i.toLowerCase(lastNames));}
+            while (Letter < 7 - count) {
+                System.out.print(lastNames[i].charAt(Letter));
+                Letter++;
+            }
+                System.out.print(lastNames[i].charAt(Letter) + "\n");
+                Letter++;
             }
         }
     }
+}
