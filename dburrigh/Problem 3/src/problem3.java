183f2c7b126d582acb6c16bf82421bd5d095b630
dburrigh <dburrigh@nebrwesleyan.edu>
2018-02-26 21:51:58 -0600

    Everything should work

--- dburrigh/Problem 3/src/problem3.java
+++ dburrigh/Problem 3/src/problem3.java
@@ -3,29 +3,32 @@ import java.util.Scanner;
 public class problem3 {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String[] firstNames = {"dallas", "bob", "j"};
-        String[] lastNames = {"burright", "johnson","life"};
+        Scanner input = new Scanner(System.in);
+        String firstNames;
+        String lastNames;
+        int n = input.nextInt();
         int i;
-        for (i = 0; i < firstNames.length; i++) {
-            System.out.print((firstNames[i].charAt(0)));
+        for (i = 0; i < n; i++) {
+            firstNames = input.next();
+            lastNames = input.next();
+            System.out.print((firstNames.charAt(0)));
             int j = 0;
             int Letter = 0;
             int count = 1;
-            while (j < Math.min(firstNames[i].length() - 1, lastNames[i].length()) && firstNames[i].charAt(j + 1) < lastNames[i].charAt(0)) {
-                System.out.print(firstNames[i].charAt(j + 1));
+            while (j < Math.min(firstNames.length() - 1, lastNames.length()) && firstNames.charAt(j + 1) < lastNames.charAt(0)) {
+                System.out.print(firstNames.charAt(j + 1));
                 j++;
                 count++;
             }
-            if(count + lastNames[i].length()<8) {
-                System.out.print(lastNames[i]);
+            if(count + lastNames.length()<8) {
+                System.out.println(lastNames);
             }else{
 
             while (Letter < 7 - count) {
-                System.out.print(lastNames[i].charAt(Letter));
+                System.out.print(lastNames.charAt(Letter));
                 Letter++;
             }
-                System.out.print(lastNames[i].charAt(Letter) + "\n");
+                System.out.println(lastNames.charAt(Letter));
                 Letter++;
             }
         }
